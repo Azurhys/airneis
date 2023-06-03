9052ad5128a659a50ad64f3b2d3af3ed25c75d5e
Romain <Romain.cascio38@gmail.com>
2023-06-03 12:04:28 +0200

    Back page recherche

--- web/src/composants/front/Recherche.jsx
+++ web/src/composants/front/Recherche.jsx
@@ -1,158 +1,199 @@
 import React, { useState } from 'react';
 import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { useProduit } from "../../hook/useProduit";
 
 const Recherche = () => {
-    const [showOptions, setShowOptions] = useState(false);
-    const [searchText, setSearchText] = useState('');
-    const [material, setMaterial] = useState('');
-    const [minPrice, setMinPrice] = useState('');
-    const [maxPrice, setMaxPrice] = useState('');
-    const [categories, setCategories] = useState([]);
-    const [inStockOnly, setInStockOnly] = useState(false);
-    const [sortBy, setSortBy] = useState('price-asc');
-  
-    const handleSearchTextChange = (event) => {
-      setSearchText(event.target.value);
-    };
-  
-    const handleMaterialChange = (event) => {
-      setMaterial(event.target.value);
-    };
-  
-    const handleMinPriceChange = (event) => {
-      setMinPrice(event.target.value);
-    };
-  
-    const handleMaxPriceChange = (event) => {
-      setMaxPrice(event.target.value);
-    };
-  
-    const handleCategoriesChange = (event) => {
-      const selectedCategories = Array.from(event.target.selectedOptions, (option) => option.value);
-      setCategories(selectedCategories);
-    };
-  
-    const handleInStockOnlyChange = (event) => {
-      setInStockOnly(event.target.checked);
-    };
-  
-    const handleSortByChange = (event) => {
-      setSortBy(event.target.value);
-    };
-  
-    const handleFilterClick = () => {
-      setShowOptions(!showOptions);
-    };
-  
-    const handleSearchClick = () => {
-      // Perform the search and filtering based on the current state values
-      // ...
-    };
+  const [produits, mettreEnAvantProduit, supprimerProduit, ajouterProduit, produitDetail, afficherDetailProduit, modifierProduit, changeProductPriority] = useProduit();
+  const [showOptions, setShowOptions] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [searchTextDescription, setSearchDescriptionText] = useState('');
+  const [material, setMaterial] = useState('');
+  const [minPrice, setMinPrice] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [inStockOnly, setInStockOnly] = useState(false);
+  const [sortBy, setSortBy] = useState('price-asc');
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
-    const handleResetClick = () => {
-      setSearchText('');
-      setMaterial('');
-      setMinPrice('');
-      setMaxPrice('');
-      setCategories([]);
-      setInStockOnly(false);
-      setSortBy('price-asc');
-    };
-    
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleDescriptionTextChange = (event) => {
+    setSearchDescriptionText(event.target.value);
+  };
+
+  const handleMaterialChange = (event) => {
+    setMaterial(event.target.value);
+  };
+
+  const handleMinPriceChange = (event) => {
+    setMinPrice(event.target.value);
+  };
+
+  const handleMaxPriceChange = (event) => {
+    setMaxPrice(event.target.value);
+  };
+
+  const handleCategoriesChange = (event) => {
+    const selectedCategories = Array.from(event.target.selectedOptions, (option) => Number(option.value));
+    setCategories(selectedCategories);
+  };
+
+  const handleInStockOnlyChange = (event) => {
+    setInStockOnly(event.target.checked);
+  };
   
-    return (
-      <Container className='m-5'>
-        <Row>
-          <Col>
-            <InputGroup>
-              <Form.Control
-                type="text"
-                placeholder="Rechercher un produit..."
-                value={searchText}
-                onChange={handleSearchTextChange}
-              />
-              <Button className='btn-brown' onClick={handleFilterClick}>Filtrer</Button>
-            </InputGroup>
+  
+
+  const handleSortByChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const handleFilterClick = () => {
+    setShowOptions(!showOptions);
+  };
+
+  const handleSearchClick = () => {
+    let filteredProducts = produits.filter((produit) =>
+      produit.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    //applique les filtres
+    if (material !== '') {
+      filteredProducts = filteredProducts.filter((produit) =>
+        produit.material.toLowerCase().includes(material.toLowerCase())
+      );
+    }
+
+    if (minPrice !== '') {
+      filteredProducts = filteredProducts.filter((produit) => produit.price >= parseInt(minPrice));
+    }
+
+    if (maxPrice !== '') {
+      filteredProducts = filteredProducts.filter((produit) => produit.price <= parseInt(maxPrice));
+    }
+
+    if (categories.length > 0) {
+      filteredProducts = filteredProducts.filter((produit) =>
+        categories.includes(produit.category_id)
+      );
+    }
+
+    if (inStockOnly) {
+      filteredProducts = produits.filter((produit) => produit.quantity > 0);
+    }
+
+    if (sortBy === 'price-asc') {
+      filteredProducts.sort((a, b) => a.price - b.price);
+    } else if (sortBy === 'price-desc') {
+      filteredProducts.sort((a, b) => b.price - a.price);
+    } else if (sortBy === 'date-asc') {
+      filteredProducts.sort((a, b) => new Date(a.date) - new Date(b.date));
+    } else if (sortBy === 'date-desc') {
+      filteredProducts.sort((a, b) => new Date(b.date) - new Date(a.date));
+    }
+    
+    //ajoute description
+    if (searchTextDescription !== '') {
+      filteredProducts = filteredProducts.filter((produit) =>
+      produit.description.toLowerCase().includes(searchTextDescription.toLowerCase())
+    );
+    }
+
+    setFilteredProducts(filteredProducts.slice(0, 3));
+  };
+
+  const handleResetClick = () => {
+    setSearchText('');
+    setMaterial('');
+    setMinPrice('');
+    setMaxPrice('');
+    setCategories([]);
+    setInStockOnly(false);
+    setSortBy('price-asc');
+    setSearchDescriptionText('');
+  };
+
+  return (
+    <Container className='m-5'>
+      <Row>
+        <Col>
+          <InputGroup>
+            <Form.Control
+              type="text"
+              placeholder="Rechercher un produit..."
+              value={searchText}
+              onChange={handleSearchTextChange}
+            />
+            <Button className='btn-brown' onClick={handleFilterClick}>Filtrer</Button>
+          </InputGroup>
+        </Col>
+      </Row>
+      {showOptions && (
+        <Row className='position-absolute start-50 w-50 filtre bg-light'>
+          <Col className='ms-auto w-50 my-2'>
+            <Button className='my-2' variant="secondary" onClick={handleResetClick}>Réinitialiser</Button>
+            <br />
+            <Form.Label className='my-2'>Description</Form.Label>
+            <Form.Control type="text" value={searchTextDescription} onChange={handleDescriptionTextChange} />
+
+            <Form.Label className='my-2'>Matériau</Form.Label>
+            <Form.Control type="text" value={material} onChange={handleMaterialChange} />
+
+            <Form.Label className='my-2'>Prix min.</Form.Label>
+            <Form.Control type="number" value={minPrice} onChange={handleMinPriceChange} />
+
+            <Form.Label className='my-2'>Prix max.</Form.Label>
+            <Form.Control type="number" value={maxPrice} onChange={handleMaxPriceChange} />
+
+            <Form.Label className='my-2'>Catégories</Form.Label>
+            <Form.Control as="select" value={categories} onChange={handleCategoriesChange}>
+              <option value={0}>Table</option>
+              <option value={1}>Étagères</option>
+              <option value={2}>ya pas de 2</option>
+              <option value={3}>Chaise</option>
+              <option value={4}>Armoire</option>
+              <option value={5}>Buffet</option>
+            </Form.Control>
+            
+
+
+            <Form.Check className='my-2' type="checkbox" label="Afficher uniquement les produits en stock" checked={inStockOnly} onChange={handleInStockOnlyChange} />
+
+            <Form.Label className='my-2'>Trier par</Form.Label>
+            <Form.Control as="select" value={sortBy} onChange={handleSortByChange}>
+              <option value="price-asc">Prix croissant</option>
+              <option value="price-desc">Prix décroissant</option>
+              <option value="date-asc">Le plus récent</option>
+              <option value="date-desc">Le plus ancien</option>
+            </Form.Control>
           </Col>
         </Row>
-        {showOptions && (
-          <Row className='position-absolute start-50 w-50 filtre bg-light' >
-            <Col  className='ms-auto w-50 my-2'>
-              <Button className='my-2' variant="secondary" onClick={handleResetClick}>Réinitialiser</Button>
-              <br />
-              <Form.Label className='my-2'>Materiau</Form.Label>
-              <Form.Control type="text" value={material} onChange={handleMaterialChange} />
-            
-              <Form.Label className='my-2'>Prix min.</Form.Label>
-              <Form.Control type="number" value={minPrice} onChange={handleMinPriceChange} />
-            
-              <Form.Label className='my-2'>Prix max.</Form.Label>
-              <Form.Control type="number" value={maxPrice} onChange={handleMaxPriceChange} />
-            
-              <Form.Label className='my-2'>Categories</Form.Label>
-              <Form.Control as="select" multiple value={categories} onChange={handleCategoriesChange}>
-                <option value="category1">Category 1</option>
-                <option value="category2">Category 2</option>
-                <option value="category3">Category 3</option>
-              </Form.Control>
-            
-              <Form.Check className='my-2'type="checkbox" label="Only show products in stock" checked={inStockOnly} onChange={handleInStockOnlyChange} />
-              <Form.Label className='my-2'>Trier par</Form.Label>
-              <Form.Control as="select" value={sortBy} onChange={handleSortByChange}>
-                <option value="price-asc">Prix min. au max.</option>
-                <option value="price-desc">Prix max. au min.</option>
-                <option value="date-asc">Le plus récent</option>
-                <option value="date-desc">Le plus ancien </option>
-                </Form.Control>
-                </Col>
-                </Row>
-                )}
-                <Row className='my-3 text-center'>
-                <Col>
-                <Button className='btn-brown' onClick={handleSearchClick}>Rechercher</Button>
-                </Col>
-                </Row>
-                <Row className='mt-5' >
-  <Col md={4} >
-    <div className="card mb-4 shadow-sm">
-      <img src="https://via.placeholder.com/300x200.png" alt="Product" />
-      <div className="card-body">
-        <h5 className="card-title">Product 1</h5>
-        <p className="card-text">Description of product 1.</p>
-        <p className="card-text">Material: Material 1</p>
-        <p className="card-text">Price: $100</p>
-        <p className="card-text"><small className="text-muted">In stock</small></p>
-      </div>
-    </div>
-  </Col>
-  <Col md={4}>
-    <div className="card mb-4 shadow-sm">
-      <img src="https://via.placeholder.com/300x200.png" alt="Product" />
-      <div className="card-body">
-        <h5 className="card-title">Product 2</h5>
-        <p className="card-text">Description of product 2.</p>
-        <p className="card-text">Material: Material 2</p>
-        <p className="card-text">Price: $200</p>
-        <p className="card-text"><small className="text-muted">Out of stock</small></p>
-      </div>
-    </div>
-  </Col>
-  <Col md={4}>
-    <div className="card mb-4 shadow-sm">
-      <img src="https://via.placeholder.com/300x200.png" alt="Product" />
-      <div className="card-body">
-        <h5 className="card-title">Product 3</h5>
-        <p className="card-text">Description of product 3.</p>
-        <p className="card-text">Material: Material 3</p>
-        <p className="card-text">Price: $300</p>
-        <p className="card-text"><small className="text-muted">In stock</small></p>
-      </div>
-    </div>
-  </Col>
-</Row>
+      )}
+      <Row className='my-3 text-center'>
+        <Col>
+          <Button className='btn-brown' onClick={handleSearchClick}>Rechercher</Button>
+        </Col>
+      </Row>
+      <Row className='mt-5'>
+        {filteredProducts.slice(0, 3).map((produit) => (
+          <Col md={4} key={produit.id}>
+            <div className="card mb-4 shadow-sm">
+              <img src={produit.image} alt="Product" />
+              <div className="card-body">
+                <h5 className="card-title">{produit.name}</h5>
+                <p className="card-text">{produit.description}</p>
+                <p className="card-text">Matériau: {produit.material}</p>
+                <p className="card-text">Prix: {produit.price}</p>
+                <p className="card-text"><small className="text-muted">{produit.inStock ? 'En stock' : 'En rupture de stock'}</small></p>
+              </div>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
-                </Container>
-    );
-}
- 
 export default Recherche;
