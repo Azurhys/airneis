1874cd20ef1ae36a1e05f0f383103e0a43e63cc8
Azurhys <123641752+Azurhys@users.noreply.github.com>
2023-07-12 10:15:30 +0200

    Clean Backoffice

--- web/src/composants/back/ProduitsGestion.jsx
+++ web/src/composants/back/ProduitsGestion.jsx
@@ -1,75 +1,38 @@
-import {React, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
 
-
-function ProduitsGestion({ categories, produits,  sortBy, sortOrder }) {
-  const [currentCategory, setCurrentCategory] = useState(null);
-  
-
-  const handleCategoryClick = (categoryId) => {
-    setTimeout(() => setCurrentCategory(categoryId), 0);
-  };
-
-  const changePriority = async (productID, newPriority) => {
-    try {
-      const response = await axios.patch(`${import.meta.env.VITE_API}produits/${productID}.json`, {
-          priority: newPriority
-      });
-
-      console.log("Priorité mise à jour avec succès", response.data);
-    } catch (error) {
-      console.error("Erreur lors de la mise à jour de la priorité : ", error);
-    }
-  };
-  
-
+const ProduitsGestion = ({ categories, produits, sortBy, sortOrder, handleSelect, handleSort, onProduitDelete, onProduitDetails, onProduitEdit, handleMettreEnAvant, selectedProduits, setSelectedProduits }) => {
   return (
-    <div className="w-100">
-                <h2 className="my-3 text-center">Gestion de la priorité des produits par catégorie</h2>
-                <br />
-                <div className="text-center">
-                    {categories.map((category) => (
-                      <button
-                        key={category.category_id}
-                        className="btn btn-brown mx-2"
-                        onClick={() => handleCategoryClick(category.category_id)}
-                      >
-                        {category.name}
-                      </button>
-                    ))}
-                </div>
-                <table className="table table-striped">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Description</th>
-            <th>Prix</th>
-            <th>Quantité</th>
-            <th>Catégorie</th>
-            <th onClick={() => handleSort('priority')}>
-                Priorité 
-                {sortBy === 'priority' ? (
+    <div className="d-flex w-100">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th></th>
+            <th onClick={() => handleSort('product_id')}>
+                ID 
+                {sortBy === 'product_id' ? (
                     sortOrder === 'asc' ? '▲' : '▼'
                 ) : (
                     '▲' 
                 )}
             </th>
-        </tr>
-    </thead>
-    <tbody>
-        {produits
-        .filter(produit => produit.category_id === currentCategory)
-        .sort((a, b) => {
-          if (sortBy === null) {
-            return a.product_id - b.product_id;
-        }
-            if (a[sortBy] > b[sortBy]) return sortOrder === 'asc' ? 1 : -1;
-            if (a[sortBy] < b[sortBy]) return sortOrder === 'asc' ? -1 : 1;
-            return 0;
-        })
-        .map((produit) => (
-            <tr key={produit.product_id}>
+            <th onClick={() => handleSort('name')}>Nom {sortBy === 'name' && <span>{sortOrder === 'asc' ? '▲' : '▼'}</span>}</th>
+            <th>Description</th>
+            <th onClick={() => handleSort('price')}>Prix {sortBy === 'price' && <span>{sortOrder === 'asc' ? '▲' : '▼'}</span>}</th>
+            <th onClick={() => handleSort('quantity')}>Quantité {sortBy === 'quantity' && <span>{sortOrder === 'asc' ? '▲' : '▼'}</span>}</th>
+            <th>Catégorie</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+        {produits.map((produit) => (
+              <tr key={produit.product_id}>
+                  <td>
+                  <input
+                    type="checkbox"
+                    checked={selectedProduits.includes(produit.product_id)}
+                    onChange={() => handleSelect(produit.product_id)}
+                  />
+                </td>
                 <td>{produit.product_id}</td>
                 <td>{produit.name}</td>
                 <td>{produit.description}</td>
@@ -77,18 +40,35 @@ function ProduitsGestion({ categories, produits,  sortBy, sortOrder }) {
                 <td>{produit.quantity}</td>
                 <td>{produit.category_id}</td>
                 <td>
-                    <p>Priorité actuelle : <span id={`priorityValue${produit.product_id}`}>{produit.priority}</span></p>
-                    <div className="d-flex flex-nowrap">
-                    <input className="form-control mx-3 w-25" type="number" id={`newPriority${produit.product_id}`} min="1" />
-                    <button className="btn btn-brown w-50" onClick={() => changePriority(produit.product_id, document.getElementById(`newPriority${produit.product_id}`).value)}>Changer la priorité</button>
-                    </div>
+                  <button className="btn btn-danger mx-2" onClick={() => onProduitDelete(produit.product_id)}>Supprimer</button>
+                  <button className="mx-2 btn btn-brown" onClick={() => onProduitDetails(produit.product_id)}>Détails</button>
+                  <button className="mx-2 btn btn-warning" onClick={() => onProduitEdit(produit.product_id)}>Modifier</button>
+                  <button onClick={() => handleMettreEnAvant(produit)} className={produit.enAvant ? "mx-2 btn btn-danger" : "mx-2 btn btn-success"}>{produit.enAvant ? "- vedette" : "+ vedette"}</button>
                 </td>
-            </tr>
-        ))}
-    </tbody>
-    </table>
+              </tr>
+            ))}
+        </tbody>
+        <tfoot>
+          <tr>
+                  <td colSpan={7}>
+                      <button className="mx-2 btn btn-success" onClick={(e) => onProduitCreate(e)}>Créer un produit</button>
+                      {selectedProduits.length > 0 && (
+                      <button className="btn btn-danger mx-2" onClick={() => {
+                          selectedProduits.forEach((produitId) => {
+                          const produitToDelete = produits.find((produit) => produit.product_id === produitId);
+                          if (produitToDelete) onProduitDelete(produitToDelete);
+                          });
+                          setSelectedProduits([]);
+                      }}>
+                          Supprimer les produits sélectionnés ({selectedProduits.length})
+                      </button>
+                      )}
+                  </td>
+              </tr>
+          </tfoot>
+      </table>
     </div>
   );
-}
+};
 
 export default ProduitsGestion;
